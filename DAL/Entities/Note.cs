0ef37470fbf244944d2a77d75bf3a4ff72a405f9
DRed <dima.redkin.1999@gmail.com>
2020-06-27 11:34:08 +0500

    Initial commit

---
+++ DAL/Entities/Note.cs
@@ -0,0 +1,22 @@
+﻿﻿using System;
+
+namespace Notes.DAL.Entities
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string Content { get; set; } = null!;
+        public DateTime DateOfLastChange { get; set; } = DateTime.Now;
+
+        public Note()
+        {
+        }
+
+        public Note(string? title, string content)
+        {
+            Title = title;
+            Content = content;
+        }
+    }
+}
---
+++ DAL/NotesDbContext.cs
@@ -0,0 +1,24 @@
+﻿﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Notes.DAL.Entities;
+using System.Linq;
+using Notes.DTO;
+
+namespace Notes.DAL
+{
+    public class NotesDbContext : DbContext
+    {
+        public DbSet<Note> Notes { get; set; } = null!;
+
+        public NotesDbContext(DbContextOptions<NotesDbContext> options)
+            : base(options)
+        {
+        }
+
+        public IEnumerable<NoteDto> GetAllNotesSortedByDateOfLastChange()
+        {
+            //сначала более поздние (по дате изменения)
+            return Notes.OrderByDescending(note => note.DateOfLastChange).Select(note => new NoteDto(note));
+        }
+    }
+}
